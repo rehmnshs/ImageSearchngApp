df2ba6556f5dc955062d5f31af1f04499d3c4be5
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2023-10-24 19:28:21 +0530

    did stuff

--- src/SecondPage.js
+++ src/SecondPage.js
@@ -70,10 +70,10 @@ export const SecondPage = () => {
     <div>
       <div className="headerSP">
        
-          <h3 onClick={handleClick}>sort by relevant </h3>
+          <h3 onClick={handleClick} className="relevant">sort by relevant </h3>
        
      
-          <h3 onClick={handleClick1}>sort by latest </h3>
+          <h3 onClick={handleClick1} className="latest">sort by latest </h3>
        
       </div>
       <ImageList item={real} username={username} />
@@ -81,8 +81,8 @@ export const SecondPage = () => {
    
     </div>
     <div className="footerSP">
-        <button onClick={handleClick3} className="btSP"><svg className="svgSP"  xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z"/></svg></button>
-        <button onClick={handleClick2} className="btSP"><svg className="svgSP" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M686-450H160v-60h526L438-758l42-42 320 320-320 320-42-42 248-248Z"/></svg></button>
+        <a onClick={handleClick3} ><p className="prev"> prev </p></a>
+        <a onClick={handleClick2} ><p className="next"> next </p></a>
       </div>
       </>
   );
