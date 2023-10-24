291b453becc43b6a0b39547a13042411df55f85a
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2023-10-24 19:40:22 +0530

    wads

--- src/Components/ImageShow.js
+++ src/Components/ImageShow.js
@@ -105,7 +105,7 @@ function ImageShow({ item, quality, username ,like,usn}) {
           
             <span>
           
-            {like===1 ?<button onClick={handleUnlikeClick}>unLike {item.alt_description}</button>:<button onClick={handleLikeClick}>{item.alt_description}</button>}
+            {like===1 ?<button onClick={handleUnlikeClick}>unLike {item.alt_description}</button>:<div onClick={handleLikeClick} className="descrip"><p className="descrip"></p></div>}
               {liked && <button>Unlike</button>}{" "}
             </span>
           </div>
